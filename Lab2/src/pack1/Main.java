aaac101d2c5c3f3b5b6ed72dbf5dd815120f5710
Yan Aniskevich <48385641+LORDLICH@users.noreply.github.com>
2019-04-22 09:27:52 +0300

    Add files via upload

--- Lab2/src/pack1/Main.java
+++ Lab2/src/pack1/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
@@ -19,6 +20,7 @@ public class Main extends Application{
         primaryStage.setTitle("Complex Number Calculator");
         GridPane root = new GridPane();
         Scene scene = new Scene(root, 520, 140);
+        HBox hBox = new HBox();
 
         root.setGridLinesVisible(false);
         root.setPadding(new Insets(5, 5, 5, 5));
@@ -60,13 +62,17 @@ public class Main extends Application{
         additionButton.setMaxWidth(Double.MAX_VALUE);
         additionButton.setMaxHeight(Double.MAX_VALUE);
         GridPane.setMargin(additionButton, new Insets(5));
-        root.add(additionButton, 0, 2,2,1);
 
         Button subtractionButton = new Button("-");
         subtractionButton.setMaxWidth(Double.MAX_VALUE);
         subtractionButton.setMaxHeight(Double.MAX_VALUE);
         GridPane.setMargin(subtractionButton, new Insets(5));
-        root.add(subtractionButton, 2, 2,3,1);
+
+        HBox.setHgrow(additionButton,Priority.ALWAYS);
+        HBox.setHgrow(subtractionButton,Priority.ALWAYS);
+
+        hBox.getChildren().addAll(additionButton,subtractionButton);
+        root.add(hBox,0, 2,5,1);
 
         ButtonHandler.additionButtonHandler(additionButton, realPartLabel,imaginaryPartLabel,
                 firstRealPart_t,firstImaginaryPart_t, secondRealPart_t,secondImaginaryPart_t);
